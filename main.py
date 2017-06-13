7b43bda414ce384726666ef0282a7d61ba53784d
Jakob Willforss <jakob.willforss@hotmail.com>
2017-06-13 10:15:41 +0200

    Function infrastructure for subpart calling in place

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 
 import argparse
 
+import preprocessing.main
+import search.main
+import postprocessing.main
+
 VERSION = '0.1.0'
 
 author = 'Jakob Willforss'
@@ -11,23 +15,31 @@ help_message = 'Author: {}\n Version: {}\n\n' \
     .format(author, VERSION)
 
 
-def db_setup():
-    pass
+def db_setup(args):
+
+    preprocessing.main.main(args)
 
 
-def run_search():
-    pass
+def run_search(args):
+
+    search.main.main(args)
 
 
-def post_process():
-    pass
+def post_process(args):
+
+    postprocessing.main.main(args)
 
 
-def run_all():
-    pass
+def run_all(args):
+
+    preprocessing.main.main(args)
+    search.main.main(args)
+    postprocessing.main.main(args)
 
 
 def parse_arguments():
+
+    print('parsing arguments')
 
     parsers = ['db_setup', 'run_search', 'post_process', 'run_all']
 
@@ -42,12 +54,18 @@ def parse_arguments():
     subparsers = parser.add_subparsers(help='Commands: {}'.format(' '.join(parsers)))
 
     parse_db_setup(subparsers, parsers[0])
+    parse_run_msfragger(subparsers, parsers[1])
+    parse_postproc(subparsers, parsers[2])
+    parse_run_all(subparsers, parsers[3])
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 def parse_run_all(subparsers, parser_name):
 
     parser = subparsers.add_parser(parser_name)
-    parser.set_defaults(func=db_setup)
+    parser.set_defaults(func=run_all)
 
 
 def parse_db_setup(subparsers, parser_name):
@@ -59,13 +77,13 @@ def parse_db_setup(subparsers, parser_name):
 def parse_run_msfragger(subparsers, parser_name):
 
     parser = subparsers.add_parser(parser_name)
-    parser.set_defaults(func=db_setup)
+    parser.set_defaults(func=run_search)
 
 
 def parse_postproc(subparsers, parser_name):
 
     parser = subparsers.add_parser(parser_name)
-    parser.set_defaults(func=db_setup)
+    parser.set_defaults(func=post_process)
 
 
 if __name__ == '__main__':
